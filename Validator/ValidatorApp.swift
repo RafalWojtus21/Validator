861771b8cea850bd9ff05c49f8ea3df8cecf1bbb
Rafał Wojtuś <rafalwojtus21@gmail.com>
2023-11-14 10:40:04 +0100

    Initial Commit

---
+++ Validator/ValidatorApp.swift
@@ -0,0 +1,17 @@
+//
+//  ValidatorApp.swift
+//  Validator
+//
+//  Created by Rafał Wojtuś on 14/11/2023.
+//
+
+import SwiftUI
+
+@main
+struct ValidatorApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
